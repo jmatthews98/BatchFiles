1b8e3757a712d9b7ee2d36eb8aa0cc2e5fd6c86c
justinmatthews <jmatthews98@tamu.edu>
2019-03-25 16:14:35 -0500

    adding some batch files

---
+++ Remote_setup.bat
@@ -0,0 +1,24 @@
+::This is to be elevated and run on a pc
+@echo off
+rem targets current directory I think
+::pushd %~dp0
+::echo ######################################################################################### >> ..\remote_list.txt
+::hostname >> ..\remote_list.txt
+::ipconfig >> ..\remote_list.txt
+::echo user list >> ..\remote_list.txt
+::dir C:\Users >> ..\remote_list.txt
+::echo remote setup finished
+::pause
+::echo # >> ..\remote_list.txt
+::echo # >> ..\remote_list.txt
+::
+net use M: /delete
+net use M: \\ECESVW30401.engr.tamu.edu\Software /user:engr\coe-justin.matthews Test1234
+cd Windows\Scripting\justin.matthews
+start .\info_grab.bat /k
+C:
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh firewall set service type = remotedesktop mode = enable
+net use M: /delete
+::pause
+::control sysdm.cpl
---
+++ info_grab.bat
@@ -0,0 +1,26 @@
+::This is to be called by remote_setup.bat and should be run off of the network drive
+@echo off
+pushd %~dp0
+echo ###################################################################################### >> ..\remote_list.txt
+
+hostname >> temphost
+set /p name= < temphost
+del temphost
+del ..\Remote_List\%name%.txt
+
+hostname >> ..\remote_list.txt
+ipconfig >> ..\remote_list.txt
+echo user list >> ..\remote_list.txt
+dir C:\Users >> ..\remote_list.txt
+
+echo # >> ..\remote_list.txt
+echo # >> ..\remote_list.txt
+
+move ..\remote_list.txt ..\Remote_List\%name%.txt
+
+::
+::reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+::netsh firewall set service type = remotedesktop mode = enable
+::pause
+::control sysdm.cpl
+exit
